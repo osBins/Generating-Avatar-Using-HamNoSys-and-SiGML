edcdcd9ef02ca8648678ee05fe46b02a2a35712a
harshitsingh1902 <76938382+harshitsingh1902@users.noreply.github.com>
2023-11-29 09:44:59 +0530

    stop btn added main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,12 @@ document.getElementById('play-button').addEventListener('click', function() {
     document.getElementById('uploaded-video').play();
     document.getElementById('cwasa-frame').contentWindow.document.getElementById('harshitbtn').click();
 });
+
+document.getElementById('stop-button').addEventListener('click', function() {
+    var video = document.getElementById('uploaded-video');
+    video.pause(); 
+    video.currentTime = 0; 
+
+    // clicking a button inside an iframe as in the play-button handler
+    document.getElementById('cwasa-frame').contentWindow.document.getElementById('yourStopButtonId').click();
+});
