694730081d465fa163ccf45232a82f2b0b2ce36e
osBins <arpitjain.2001@gmail.com>
2023-11-29 10:06:46 +0530

    stop button to stop CWASA

--- static/js/main.js
+++ static/js/main.js
@@ -6,6 +6,7 @@ document.getElementById('play-button').addEventListener('click', function() {
 document.getElementById('stop-button').addEventListener('click', function() {
     var video = document.getElementById('uploaded-video');
     video.pause(); 
+    document.getElementById('cwasa-frame').contentWindow.document.getElementById('cwasaStop').click();
     video.currentTime = 0; 
 
     // clicking a button inside an iframe as in the play-button handler
